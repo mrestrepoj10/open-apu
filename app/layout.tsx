bac5aa8eb6fb4f218df0378cb0978f207ecc4f25
MR <mrestrepoj10@gmail.com>
2026-08-03 16:45:58 -0500

    Update global styles and layout configuration
    Refactor global CSS variables for improved color management and add shadow properties. Update layout component to use a new font variable and change the document language from English to Spanish.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,12 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const fontSans = Geist({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -18,9 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html
-      lang="en"
+      lang="es"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn("antialiased", "font-sans", fontSans.variable, fontMono.variable)}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
